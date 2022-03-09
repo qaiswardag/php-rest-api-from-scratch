00adde93c4d8197cc3be0e0bdd3ccd22d1c21c96
Qais Wardag <47112831+qaiswardag@users.noreply.github.com>
2022-03-09 17:15:52 +0100

    Add index.php

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Title</title>
+</head>
+<body>
+<div class="container">
+    <h1>This is a Heading</h1>
+    <p>This is a paragraph.</p>
+</div>
+</body>
+</html>
+
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,200;0,300;0,400;0,500;0,600;1,200;1,300;1,400;1,500;1,600&display=swap');
+
+    * {
+        font-family: 'Jost', sans-serif;
+    }
+
+    body {
+        max-width: 1000px;
+        margin: 50px auto 50px auto;
+        padding: 50px 20px;
+    }
+
+    .container {
+        padding: 50px 50px;
+        border: 1px solid #000;
+    }
+
+    hr {
+        margin: 40px 0;
+        height: 2px;
+        background: #f35656;
+        padding: 0;
+        border: none;
+    }
+
+
+    .product-img {
+        width: 50px;
+    }
+
+    .product-img-view {
+        width: 120px;
+    }
+
+    .back {
+        background: black;
+    }
+</style>
